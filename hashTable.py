084302b7f98eda85f11c3c628d5d56172ac252d5
Prafulla Pradhan <ppradhan22638@ucumberlands.edu>
2024-09-23 22:42:49 -0500

    ProjectPhase2: Added hashTable.py

---
+++ hashTable.py
@@ -0,0 +1,30 @@
+class HashTable:
+    def __init__(self, size=1000):
+        self.size = size
+        self.table = [[] for _ in range(self.size)]
+
+    def _hash(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        index = self._hash(key)
+        for item in self.table[index]:
+            if item[0] == key:
+                item[1] = value
+                return
+        self.table[index].append([key, value])
+
+    def get(self, key):
+        index = self._hash(key)
+        for item in self.table[index]:
+            if item[0] == key:
+                return item[1]
+        raise KeyError(key)
+
+    def delete(self, key):
+        index = self._hash(key)
+        for i, item in enumerate(self.table[index]):
+            if item[0] == key:
+                del self.table[index][i]
+                return
+        raise KeyError(key)
